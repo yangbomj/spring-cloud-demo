e288e10c969b8d5d70520b42b7fc709d8cc8074f
yangbomj <718873923@qq.com>
2021-05-17 22:49:19 +0800

    代码tijiao

---
+++ com.user.demo/src/main/java/com/user/demo/config/NacosConfig.java
@@ -0,0 +1,4 @@
+package com.user.demo.config;
+
+public class NacosConfig {
+}
--- com.user.demo/src/main/java/com/user/demo/service/UserService.java
+++ com.user.demo/src/main/java/com/user/demo/service/UserService.java
@@ -1,4 +1,6 @@
 package com.user.demo.service;
 
-public interface UserService {
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface UserService extends IService {
 }
